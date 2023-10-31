5c1ed771c53f27bff5ff36414d7dd985b9050ab8
Manuel Buil <mbuil@suse.com>
2023-10-31 13:04:23 +0100

    Refactor Terraform
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- 2022/terraform/new/changeIPSshConfig.sh
+++
@@ -1,8 +0,0 @@
-ip0=$(terraform output -json | jq '.ipAddresses.value[0]')
-ip1=$(terraform output -json | jq '.ipAddresses.value[1]')
-
-echo $ip0
-echo $ip1
-
-sed -i '/^Host azure-ubuntu/{n;s/Hostname .*/Hostname '$ip0'/}' ~/.ssh/config
-sed -i '/^Host azure-ubuntu2/{n;s/Hostname .*/Hostname '$ip1'/}' ~/.ssh/config
---
+++ 2022/terraform/new/installK3sAndRancher.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+apt update
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+cat <<EOF > config.yaml
+write-kubeconfig-mode: 644
+token: "secret"
+EOF
+
+mkdir -p /etc/rancher/k3s
+cp config.yaml /etc/rancher/k3s/config.yaml
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.26.9+k3s1 sh -
+sleep 30
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+kubectl create namespace cattle-system
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.2/cert-manager.crds.yaml
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace
+sleep 20
+myIP=$(curl ifconfig.me)
+helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=$myIP.sslip.io --set replicas=1 --set bootstrapPassword=linux
