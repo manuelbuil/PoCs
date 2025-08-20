6dfa099834fe61c55ead00956aa8ab15a1727e8f
manuelbuil <mbuil@suse.com>
2025-08-20 13:07:15 +0200

    Fix logging
    Signed-off-by: manuelbuil <mbuil@suse.com>

--- 2024/HTMLParser/start-parser.sh
+++ 2024/HTMLParser/start-parser.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Remove the mail so that it does not get huge
-rm /var/spool/mail/manuel
-
 # Activate the Python virtual environment
 source /home/manuel/go/src/github.com/manuelbuil/PoCs/2024/HTMLParser/sego_env/bin/activate
 
-# Run your Python script
-python /home/manuel/go/src/github.com/manuelbuil/PoCs/2024/HTMLParser/miparser.py 
+# Run your Python script | sudo journalctl -t MyPythonScript to check the logs
+python /home/manuel/go/src/github.com/manuelbuil/PoCs/2024/HTMLParser/miparser.py 2>&1 | logger -t MyPythonScript
 
 # Deactivate the virtual environment (optional)
 deactivate
