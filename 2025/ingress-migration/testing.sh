ba4e8eb5c9131eeedb147c7c43c9129eb03c4c25
Manuel Buil <mbuil@suse.com>
2025-12-02 22:53:25 +0100

    Improve testing.sh
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- 2025/ingress-migration/testing.sh
+++ 2025/ingress-migration/testing.sh
@@ -17,16 +17,49 @@ compare() {
     fi
 }
 
+
+IP=$1
+
+if [ -z "$IP" ]; then
+    echo "❌ Error: The variable IP cannot be empty." >&2
+    exit 1
+fi
+
 kubectl create secret tls dummy-tls-secret --cert=dummy-tls.crt --key=dummy-tls.key --namespace test-migration
-IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}' | cut -d" " -f1)
-OUTPUT1=$($CURL -H "Host: simple.example.com" http://$IP)
-compare $OUTPUT1 "200" "simple.example.com"
-OUTPUT2=$($CURL -H "Host: annotations.example.com" http://$IP/app/test)
-compare $OUTPUT2 "401" "auth annotations no auth"
-OUTPUT3=$($CURL -u "user:password" -H "Host: annotations.example.com" http://$IP/app/test)
-compare $OUTPUT3 "200" "auth annotations with auth"
-OUTPUT4=$($CURL -u "user:password" -H "Host: another.nonworking.annotation.example.com" http://$IP/app/test)
-compare $OUTPUT4 "200" "redirect"
-OUTPUT5=$($CURL -H "Host: oneannotation.example.com" http://$IP/)
-compare $OUTPUT5 "308" "ssl-redirect"
-curl -v -H "Host: nonworking.example.com" http://$IP/
+kubectl create ns test-migration
+echo "user:$(echo -n password | openssl passwd -stdin -apr1)" > auth_file.txt
+kubectl create secret generic basic-auth-secret --from-file=auth=auth_file.txt -n test-migration
+rm auth_file.txt
+
+echo
+echo "========================== STARTING TEST ================================="
+echo
+
+# TEST1 - SIMPLE APP
+OUTPUT_TEST1=$($CURL -H "Host: simple.example.com" http://$IP)
+compare $OUTPUT_TEST1 "200" "simple.example.com (should work)"
+
+# TEST2 - AUTH ANNOTATIONS
+OUTPUT_TEST2A=$($CURL -H "Host: auth.example.com" http://$IP)
+compare $OUTPUT_TEST2A "401" "auth.example no auth (should work)"
+OUTPUT_TEST2B=$($CURL -u "user:password" -H "Host: auth.example.com" http://$IP)
+compare $OUTPUT_TEST2B "200" "auth.example with auth (should work)"
+
+# TEST3 - REWRITE PATH
+OUTPUT_TEST3=$($CURL -H "Host: nonworking.rewrite.example.com" http://$IP/app/test)
+compare $OUTPUT_TEST3 "200" "rewrite (only works with nginx)"
+
+# TEST4 - COOKIES
+OUTPUT_TEST4=$(curl -s -c cookie-jar.txt -H "Host: cookie.example.com" http://$IP)
+for i in {1..5}; do curl -s -b cookie-jar.txt -H "Host: cookie.example.com" http://$IP | grep Hostname: ; sleep 0.5; done
+
+# TEST5 - REDIRECT
+OUTPUT5=$($CURL -H "Host: ssl.redirect.example.com" http://$IP/)
+compare $OUTPUT5 "308" "ssl-redirect (should work)"
+
+# TEST6 - UPSTREAM VHOST
+if curl -s -H "Host: nonworking.upstreamvhost.example.com" http://$IP/ | grep -q isitworking; then
+    compare "good" "good" "vhost (only works with nginx)"
+else
+    compare "good" "bad" "vhost (only works with nginx)"
+fi
